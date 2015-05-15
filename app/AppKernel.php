60fc9a29d2ae5d9724e610853691bc6d00c19e6b
Sebastien Maloron <contact@smaloron.eu>
2015-05-15 16:09:07 +0200

    Moved AppBundle to Darkroom/AppBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,8 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new Darkroom\ModelBundle\DarkroomModelBundle(),
+            new Darkroom\AppBundle\DarkroomAppBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
